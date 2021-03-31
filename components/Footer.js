dc9ed53216ba87cf6edde900db7eeeed07879d51
Anders Norrback Bornholm <anders@bornholm.se>
2021-03-31 17:52:19 +0200

    Footer with logos

--- components/Footer.js
+++ components/Footer.js
@@ -1,44 +1,29 @@
 import React, { useContext } from "react";
 
-import FooterLogo from "../assets/images/logo-1-1.png";
+import AllmannyttanLogo from "../assets/images/resources/sveriges-allmannytta.jpg";
+import NikolaiLogo from "../assets/images/resources/nikolai.png";
+import IteamLogo from "../assets/images/resources/iteam.png";
 
 const Footer = () => {
   return (
     <div>
-      {/* <section className="mailchimp-one">
-        <div className="container wow fadeInUp">
-          <div className="inner-container">
-            <div className="mailchimp-one__icon">
-              <i className="apton-icon-mail"></i>
-            </div>
-            <form action="#" className="mailchimp-one__form">
-              <input
-                type="text"
-                placeholder="Enter your email address"
-                name="email"
-              />
-              <button className="thm-btn mailchimp-one__btn" type="submit">
-                <span>Register Now</span>
-              </button>
-            </form>
-          </div>
-        </div>
-      </section> */}
-
-      <footer className="site-footer">
+      <footer className="site-footer" id="footer">
         <div className="site-footer__upper">
+          <p>Vi som står bakom</p>
+
           <div className="container">
             <div className="row">
-              <div className="col-lg-4">
+              <div className="col-lg-12">
                 <div className="footer-widget footer-widget__about">
-                <a href="/">
-                    <img src={FooterLogo} width="180" alt="" />
+                  <a href="/">
+                    <img src={AllmannyttanLogo} width="180" alt="" />
                   </a>
-                  {/* <p>
-                    Lorem Ipsum is simply dummy text the <br /> printing and
-                    setting industry. Lorm Ipsum
-                    <br /> has been the text ever.
-                  </p> */}
+                  <a href="/">
+                    <img src={IteamLogo} width="180" alt="" />
+                  </a>
+                  <a href="/">
+                    <img src={NikolaiLogo} width="180" alt="" />
+                  </a>
                 </div>
               </div>
 
@@ -97,36 +82,33 @@ const Footer = () => {
                   </div>
                 </div>
               </div> */}
-              <div className="col-lg-3 d-flex align-items-center justify-content-xl-end justify-content-lg-end justify-content-md-center justify-content-sm-center">
-                <div className="footer-widget">
-                  <div className="footer-widget__social">
-                    {/* <a href="">
-                      <i className="fab fa-youtube"></i>
-                    </a>
-                    <a href="">
-                      <i className="fab fa-twitter"></i>
-                    </a> */}
-                    {/* <a href="#">
-                      <i className="fab fa-my"></i>
-                    </a> */}
-                    <a href="https://www.linkedin.com/company/allm%C3%A4nnyttans-digitaliseringsinitiativ/">
-                      <i className="fab fa-linkedin"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/iteam1337/">
-                      <i className="fab fa-linkedin"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/nikolaidev/">
-                      <i className="fab fa-linkedin"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
         <div className="site-footer__bottom">
-          <div className="container text-center">
-            <p>© copyright 2021 by API1st.org</p>
+          <div className="col-lg-12 d-flex align-items-center justify-content-md-center justify-content-sm-center">
+            <div className="footer-widget">
+              <div className="footer-widget__social">
+                {/* <a href="">
+                  <i className="fab fa-youtube"></i>
+                </a>
+                <a href="">
+                  <i className="fab fa-twitter"></i>
+                </a> */}
+                {/* <a href="#">
+                  <i className="fab fa-my"></i>
+                </a> */}
+                <a href="https://www.linkedin.com/company/allm%C3%A4nnyttans-digitaliseringsinitiativ/">
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="https://www.linkedin.com/company/iteam1337/">
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="https://www.linkedin.com/company/nikolaidev/">
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
