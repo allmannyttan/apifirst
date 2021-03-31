606faaa2abdcc6dff107459f3052eaa97a702ac3
Christian Landgren <christian@landgren.nu>
2021-03-31 17:09:51 +0200

    feat: image

--- components/CTATwo.js
+++ components/CTATwo.js
@@ -1,9 +1,7 @@
 import React from "react";
 import BlockTitle from "./BlockTitle";
 
-import CTAImage1 from "../assets/images/resources/cta-2-moc-1.png";
-import CTAImage2 from "../assets/images/resources/cta-2-moc-2.png";
-import CTAImage3 from "../assets/images/resources/cta-2-moc-3.png";
+import CTAImage1 from "../assets/images/shapes/apifirst.png";
 
 const CTATwo = () => {
   return (
@@ -32,12 +30,6 @@ const CTATwo = () => {
             <div className="cta-two__images d-flex justify-content-end align-items-end flex-column">
               <img
                 src={CTAImage1}
-                className="wow fadeInUp"
-                data-wow-duration="1500ms"
-                alt=""
-              />
-              <img
-                src={CTAImage3}
                 className="wow fadeInUp"
                 data-wow-duration="1500ms"
                 alt=""
