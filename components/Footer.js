0ba6f5db72daa5db7db8dcb9cedaca1795a4dbcb
Mattias Axell <mattias.axell@metasolutions.se>
2022-04-01 11:49:01 +0200

    add MetaSolutions, Open Knowledge Sweden, Handlingar.se

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,9 @@ import SkilledLogo from "../assets/images/resources/skilled.png";
 import AlingsaskommunLogo from "../assets/images/resources/alingsaskommun.svg";
 import SwedishJobtechLogo from "../assets/images/resources/swedishjobtech.png";
 import AssedonLogo from "../assets/images/resources/assedon.png";
-
+import MetaSolutionsLogo from "../assets/images/resources/metasolutions.webp";
+import OpenKnowledgeSwedenLogo from "../assets/images/resources/openknowledgesweden.webp";
+import HandlingarLogo from "../assets/images/resources/handlingar.webp";
 
 const Footer = () => {
   return (
@@ -58,6 +60,15 @@ const Footer = () => {
                   </a>
                   <a href="https://www.roaring.io/en/">
                     <img src="https://www.roaring.io/wp-content/uploads/2021/11/roaring-logo-green-1.svg" width="180" alt="" />
+                  </a>
+                  <a href="https://www.metasolutions.se/">
+                    <img src={MetaSolutionsLogo} width="180" alt="MetaSolutions Logo" />
+                  </a>
+                  <a href="https://okfn.se/">
+                    <img src={OpenKnowledgeSwedenLogo} width="180" alt="OpenKnowledgeSweden Logo" />
+                  </a>
+                  <a href="https://handlingar.se/">
+                    <img src={HandlingarLogo} width="180" alt="Handlingar Logo" />
                   </a>
                   {
                   // add your logo here, thnx:
